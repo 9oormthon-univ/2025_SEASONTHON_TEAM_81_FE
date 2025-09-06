d604198bdf1a4a8de0969347d163ea970c777d9d
kjyook <kjyook01@gmail.com>
2025-09-06 10:49:00 +0900

    fix: home page ui

--- src/components/ui/grow-bar-graph.tsx
+++ src/components/ui/grow-bar-graph.tsx
@@ -1,4 +1,5 @@
 import calculatePercentage from '@/lib/percentage';
+import Image from 'next/image';
 
 interface GrowBarGraphProps {
   grow: number;
@@ -10,16 +11,36 @@ const GrowBarGraph = ({ grow }: GrowBarGraphProps) => {
   return (
     <div className="absolute bottom-0 left-[1.5rem] flex flex-col items-center justify-center">
       <div className="">
-        <div className="relative flex flex-col items-center h-45 w-5 rounded-[1.25rem] bg-[#BFDDBF]">
+        <div className="relative flex flex-col items-center h-45 w-5 rounded-[1.25rem] bg-white">
           <div
-            className="absolute bottom-0 w-[0.625rem] mb-2 max-h-41 rounded-[1.25rem] bg-[#4DB960]"
+            className="absolute bottom-0 w-[0.625rem] mb-2 max-h-41 rounded-[1.25rem] bg-gray5"
             style={{ height: `${growPercentage}%` }}
           />
         </div>
       </div>
+      <div className="absolute flex flex-col items-center justify-between top-0 left-0 w-1/3 pb-10 h-full">
+        <Image
+          src="/icon/graph_flower.svg"
+          alt="flower"
+          width={14}
+          height={14}
+        />
+        <Image
+          src="/icon/graph_plant.svg"
+          alt="plant"
+          width={14}
+          height={14}
+        />
+        <Image
+          src="/icon/graph_seed.svg"
+          alt="seed"
+          width={14}
+          height={14}
+        />
+      </div>
       <div className="flex flex-col items-center justify-center label7 text-center text-[#707070]">
-        <p>{grow}</p>
         <p>성장 포인트</p>
+        <p>{grow}</p>
       </div>
     </div>
   );
